ada263dc42be1b4da9d7dc56689add8454f1d4bc
Craig Carlson <dark.gkyaun@gmail.com>
2020-05-05 14:04:51 -0500

    Built and prefabbed testing tips

---
+++ Assets/Scripts/TutorialTip.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TutorialTip : MonoBehaviour {
+
+    [SerializeField] string message;
+
+    private void OnTriggerEnter2D (Collider2D other) {
+        if (other.tag == "Player") {
+            EventsManager.BroadcastMessage (message);
+            Destroy (gameObject);
+        }
+    }
+}
