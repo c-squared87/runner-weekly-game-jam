cd30b807fddbac15a516eaf261c810d09b053137
Craig Carlson <dark.gkyaun@gmail.com>
2020-05-05 10:31:16 -0500

    Cleaned up prefabs and testlevel heirarchy

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,9 @@ public class CameraFollow : MonoBehaviour {
 
     Player target;
 
+    [SerializeField] float x_offset;
+    [SerializeField] float y_offset;
+
     public void SetTarget (Player _target) {
         target = null;
         target = _target;
@@ -14,7 +17,8 @@ public class CameraFollow : MonoBehaviour {
     private void LateUpdate () {
         Vector3 _pos = target.transform.position;
         _pos.z = -10;
-        _pos.y = 0;
+        _pos.y = y_offset;
+        _pos.x += x_offset;
         transform.position = _pos;
     }
 }
