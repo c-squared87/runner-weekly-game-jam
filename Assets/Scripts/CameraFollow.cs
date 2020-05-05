71504514869ae6605d0db8e0bdb9b1037b37156d
Craig Carlson <dark.gkyaun@gmail.com>
2020-05-05 11:25:17 -0500

    Added Camera scroll back feature.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,16 +9,32 @@ public class CameraFollow : MonoBehaviour {
     [SerializeField] float x_offset;
     [SerializeField] float y_offset;
 
+    [SerializeField] float moveSpeed;
+
     public void SetTarget (Player _target) {
         target = null;
         target = _target;
     }
 
+    public void ClearTarget () {
+        target = null;
+    }
+
     private void LateUpdate () {
-        Vector3 _pos = target.transform.position;
+        Vector3 _pos;
+
+        if (target != null) {
+            _pos = target.transform.position;
+        } else {
+            _pos = Vector3.zero;
+        }
+
         _pos.z = -10;
         _pos.y = y_offset;
         _pos.x += x_offset;
-        transform.position = _pos;
+
+        float step = moveSpeed * Time.deltaTime; // calculate distance to move
+
+        transform.position = Vector3.MoveTowards (transform.position, _pos, step);
     }
 }
